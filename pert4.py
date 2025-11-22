1106edf997f98b168c9e0dd363e7f24b1472a8f9
ariyaalfarizi5-hub <ariyaalfarizi5@gmail.com>
2025-11-22 12:13:23 +0800

    Add files via upload

---
+++ pert4.py
@@ -0,0 +1,13 @@
+# Mengambil input Nama dari user
+
+nama = "Arya Alfarizi" # tipe data string (tulisan "str")
+print("nilai dari nama :", nama)
+print(type(nama))
+
+nim = "25241106" # tipe data nim string (tulisan str)
+print("nilai dari nim :", nim)
+print(type(nim))
+
+nim = 25241106 # tipe data benar salah (True / nim "boll")
+print("nilai dari nim :", nim)
+print(type(nim))
