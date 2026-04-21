5efae58795a28d3be702db9d990e267ca3848cd6
ariyaalfarizi5-hub <ariyaalfarizi5@gmail.com>
2026-04-21 12:20:31 +0800

    Part1.py

--- Struktur Data/Part1.py
+++ Struktur Data/Part1.py
@@ -9,6 +9,7 @@ if umur == 21:
 elif umur == 0 :
     print("anda belum lahir")
 elif umur == 15 :
-    print("anda be;u, cukup umur")
+    print("anda belum cukup umur")
 elif umur == 75 :
     print("anda sudah bau tanah perbanyak ibadah biar masuk surga")
+print ("Program selesai")
