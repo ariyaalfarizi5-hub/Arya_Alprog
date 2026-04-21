8325e79e49a81aeca5c9f1c32a470745e6ed53ac
ariyaalfarizi5-hub <ariyaalfarizi5@gmail.com>
2026-04-21 12:04:27 +0800

    Add files via upload

---
+++ Struktur Data/Part1.py
@@ -0,0 +1,14 @@
+nama = input("masukkan nama anda:")
+
+if nama== "arya" :
+    print("selamat datang arya")
+umur = int(input("masukkan umur anda:"))
+
+if umur == 21:
+    print("anda cukup umur")
+elif umur == 0 :
+    print("anda belum lahir")
+elif umur == 15 :
+    print("anda be;u, cukup umur")
+elif umur == 75 :
+    print("anda sudah bau tanah perbanyak ibadah biar masuk surga")
